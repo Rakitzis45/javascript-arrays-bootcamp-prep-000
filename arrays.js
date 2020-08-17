17091d3ef74538bae8c36e23e19ef1f60ddb2b88
Michael Rakitzis <michael.rakitzis@gmail.com>
2020-08-17 17:28:21 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles",];
-function addElementToBeginningOfArray(array, "element"){
-  return array.unshift();
+function addElementToBeginningOfArray(array, element){
+  return chocolateBars.unshift();
 }
 addElementToBeginningOfArray(chocolateBars, "Hersheys");
 console.log(chocolateBars);
